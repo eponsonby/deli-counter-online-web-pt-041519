79ccfc1f30d620324991efecdace1144b8c22dee
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 14:00:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(deli_line)
     puts "The line is currently empty."
   elsif deli_line.length > 0
     position_number = 1
-    positions = "The line is currently: "
+    positions = "The line is currently:"
     deli_line.each do |name|
     positions << " #{position_number}. #{name}"
     position_number += 1
